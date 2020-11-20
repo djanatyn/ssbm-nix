80fea8fd2f7cc814c8de54419ac1e5193f8e7c3e
Jonathan Strickland <djanatyn@gmail.com>
2020-11-20 12:56:37 -0500

    build an ISO (but dolphin doesn't like it)

--- uncle-punch/default.nix
+++ uncle-punch/default.nix
@@ -16,16 +16,30 @@ stdenv.mkDerivation rec {
     patch -u -p1 'Build TM Codeset/codes.json' < ${./fix-codes.patch}
   '';
 
-  buildInputs = [ ssbm wiimms-iso-tools gecko xdelta powerpc-eabi-assembling ];
+  buildInputs =
+    [ ssbm wiimms-iso-tools gecko xdelta powerpc-eabi-assembling xxd ];
 
   buildPhase = ''
     (cd 'Build TM Codeset' && gecko build)
 
-    wit extract ${ssbm}/ssbm.iso $out/ssbm-unpacked
-    # xdelta3 -d -s $out/ssbm-unpackedk
+    wit extract ${ssbm}/ssbm.iso ssbm-unpacked
+
+    # main.dol (wit) -> Start.dol (GC-Rebuilder)
+    xxd ssbm-unpacked/P-GALE/sys/main.dol > wit.hex
+    patch -p0 < ${./fix-start-dol.patch}
+    xxd -r wit.hex > Start.dol
+
+    xdelta3 -d -f \
+      -s Start.dol \
+      'Build TM Start.dol/StartdolTMPatch.xdelta' \
+      ssbm-unpacked/P-GALE/sys/Start.dol
+
+    cp -rv 'Additional ISO Files/' ssbm-unpacked/P-GALE/files
+
+    wit copy ssbm-unpacked uncle-punch.iso
   '';
 
   installPhase = ''
-    install -Dm755 Additional\ ISO\ Files/codes.gct -t $out
+    install -Dm755 uncle-punch.iso -t $out/game
   '';
 }
