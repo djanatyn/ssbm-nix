2fe9e08059a8151fad0662d6dc881975d514c00a
Jonathan Strickland <djanatyn@gmail.com>
2020-11-20 08:33:17 -0500

    uncle-punch: try wit extract

--- uncle-punch/default.nix
+++ uncle-punch/default.nix
@@ -19,7 +19,11 @@ stdenv.mkDerivation rec {
   buildInputs = [ ssbm wiimms-iso-tools gecko xdelta powerpc-eabi-assembling ];
 
   buildPhase = ''
-    cd Build\ TM\ Codeset && gecko build
+    cd 'Build TM Codeset'
+    gecko build
+
+    wit extract ${ssbm}/ssbm.iso $out/ssbm-unpacked
+    # xdelta3 -d -s $out/ssbm-unpackedk
   '';
 
   installPhase = ''
