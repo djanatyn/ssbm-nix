0c0d5f936a4f99918e5597ed8d3a2b9db4fde445
Jonathan Strickland <djanatyn@gmail.com>
2022-03-12 10:10:27 -0500

    libudev renamed to udev
    error: 'libudev' has been renamed to/replaced by 'udev'

--- slippi/default.nix
+++ slippi/default.nix
@@ -1,7 +1,7 @@
 { stdenv, lib, makeDesktopItem, gcc, slippi-desktop, playbackSlippi, fetchFromGitHub, makeWrapper
 , mesa_drivers, mesa_glu, mesa, pkgconfig, cmake, bluez, ffmpeg, libao, libGLU
 , gtk2, gtk3, wrapGAppsHook, glib, glib-networking, gettext, xorg, readline, openal, libevdev, portaudio, libusb
-, libpulseaudio, libudev, gnumake, wxGTK30, gdk-pixbuf, soundtouch, miniupnpc
+, libpulseaudio, udev, gnumake, wxGTK30, gdk-pixbuf, soundtouch, miniupnpc
 , mbedtls, curl, lzo, sfml, enet, xdg_utils, hidapi, webkit, vulkan-loader }:
 let
 
@@ -107,7 +107,7 @@ in stdenv.mkDerivation rec {
     portaudio
     libusb
     libpulseaudio
-    libudev
+    udev
     gnumake
     wxGTK30
     gtk2
